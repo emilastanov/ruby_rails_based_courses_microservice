b641e044bf36faaea5b7faa2236a5a0aa326334e
emilastanov <emila1998@me.com>
2022-05-25 17:19:23 +0300

    routes.rb added

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+def make_crud_routes(controller, cd_only: false)
+  methods = if cd_only
+              %w(post delete)
+            else
+              %w(get post patch delete)
+            end
+
+  methods.each do |method|
+    send(method, controller, to: "#{controller}##{method}")
+  end
+end
+
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/'
+  mount Rswag::Api::Engine => '/'
+
+  make_crud_routes :courses
+end
